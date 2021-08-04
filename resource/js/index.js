a75ea648ed78b68866ad3ab863484b47cc6c6de2
이성현 <74307591+kiki3700@users.noreply.github.com>
2021-08-04 20:24:49 +0900

    Add files via upload

--- resource/js/index.js
+++ resource/js/index.js
@@ -1,3 +1,5 @@
+//화면 스크롤 펑션
+
 var indexBtn = document.querySelectorAll(".index-btn");
 var sectionList = document.querySelectorAll(".section");
 
@@ -20,3 +22,37 @@ for(let i = 0; i<arr.length; i++){
 
     })
 }
+
+
+// 모달 창 펑션
+const detailsBtn = document.querySelectorAll(".pofol-detail-btn");
+const details = document.querySelectorAll(".pofol-detail");
+const closeBtn = document.querySelectorAll(".close-btn")
+let detailArrBtn = Array.from(detailsBtn)
+let detailArr = Array.from(details)
+const closeArr = Array.from(closeBtn)
+console.log(detailArr[0])
+
+for(let i=0; i<detailArrBtn.length; i++){
+    detailArrBtn[i].addEventListener("click",function(){
+        detailArr[i].style.display="block"
+        document.querySelector("body").style.overflow = 'hidden';
+        document.querySelector("body").style.backgroundColor="rgb(145, 145, 145)"
+        document.querySelector("nav").style.backgroundColor="rgb(145, 145, 145)"
+        document.querySelector("nav").style.zIndex=6
+        document.querySelector("body").style.color="rgb(112, 112, 112)"
+        detailArr[i].style.color="#000"
+    })
+}
+
+for(let i = 0 ; i<closeArr.length; i++){
+    closeArr[i].addEventListener("click",function(){
+        detailArr[i].style.display="none"
+        document.querySelector("body").style.opacity="1"
+        document.querySelector("body").style.overflow = 'scroll';
+        document.querySelector("nav").style.backgroundColor="#fff"
+        document.querySelector("body").style.backgroundColor="rgba(255,255,255)"
+        document.querySelector("body").style.color="rgba(0,0,0)"
+    })
+}
+
