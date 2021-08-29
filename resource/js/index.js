50199657e9b7d2ae02fb15cd24f20097bcf29be6
kiki3700 <5575492q@gmail.com>
2021-08-29 20:06:35 +0900

    media query

--- resource/js/index.js
+++ resource/js/index.js
@@ -1,27 +1,30 @@
+
 //화면 스크롤 펑션
 
 var indexBtn = document.querySelectorAll(".index-btn");
 var sectionList = document.querySelectorAll(".section");
 
-var about = document.querySelector(".section");
-console.log(about)
-var length = about.getBoundingClientRect()
-console.log(length)
-var x = length.y
-const a = document.querySelector(".index-btn")
+// var about = document.querySelector(".section");
+// console.log(about)
+// var length = about.getBoundingClientRect()
+// console.log(length)
+// var x = length.y
+// const a = document.querySelector(".index-btn")
 
-console.log(x);
+// console.log(x);
 
 let arr = Array.from(indexBtn);
 let arr1 = Array.from(sectionList)
 for(let i = 0; i<arr.length; i++){
     arr[i].addEventListener("click",function(){
         window.scrollTo(0,-window.innerHeight/4+window.pageYOffset+arr1[i].getBoundingClientRect().top)
-        console.log(-window.innerHeight/4+window.pageYOffset+arr1[i].getBoundingClientRect().top)
-
-
+        //window.scrollTo(0,-window.innerHeight/4+arr1[i].getBoundingClientRect().top)
+        console.log("내부높이 : "+window.innerHeight/4);
+        console.log("페이지 오프셋 : "+window.pageYOffset);
+        console.log("arr i : "+arr1[i].getBoundingClientRect().top);
     })
 }
+
 
 
 // 모달 창 펑션
