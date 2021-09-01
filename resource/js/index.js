22860d128429d644b02797b546e6cc1d7cfd9e23
kiki3700 <5575492q@gmail.com>
2021-09-02 00:38:53 +0900

    first release

--- resource/js/index.js
+++ resource/js/index.js
@@ -8,9 +8,7 @@ const closeBtn = document.querySelectorAll(".close-btn")
 let detailArrBtn = Array.from(detailsBtn)
 let detailArr = Array.from(details)
 const closeArr = Array.from(closeBtn)
-console.log(detailArr[0])
 
-console.log(window.innerWidth)
 
 for(let i=0; i<detailArrBtn.length; i++){
     detailArrBtn[i].addEventListener("click",function(){
@@ -41,16 +39,14 @@ const openBtn = document.querySelector("#open-menu");
 const nav =document.querySelector('.topnav')
 const menu = document.querySelector("#menu");
 
-console.log(menu.style.display)
+
 
 openBtn.addEventListener("click", function(){
-    console.log(menu.style.display=="")
     if(menu.style.display=="" || menu.style.display=='none'){
         menu.style.backgroundColor="#121212"
         nav.style.backgroundColor="#121212 !important"
         openBtn.style.backgroundColor="#121212"
         document.querySelector("body").style.overflow = 'hidden';
-        console.log("색"+nav.style.backgroundColor)
 
 
         menu.style.display='flex'
@@ -62,7 +58,6 @@ openBtn.addEventListener("click", function(){
        nav.style.backgroundColor="#00000000 !important";
        document.querySelector("body").style.overflow = 'scroll';
     }
-    console.log(menu.style.display)
 })
 
 
@@ -72,13 +67,10 @@ var indexBtn = document.querySelectorAll(".index-btn");
 var sectionList = document.querySelectorAll(".section");
 
 var about = document.querySelector(".section");
-console.log(about)
 var length = about.getBoundingClientRect()
-console.log(length)
 var x = length.y
 const a = document.querySelector(".index-btn")
 
-console.log(x);
 
 let arr = Array.from(indexBtn);
 let arr1 = Array.from(sectionList)
@@ -86,7 +78,6 @@ for(let i = 0; i<arr.length; i++){
     arr[i].addEventListener("click",function(){
         document.querySelector("body").style.overflow = 'scroll';
         window.scrollTo(0,-window.innerHeight/4+window.pageYOffset+arr1[i].getBoundingClientRect().top)
-        console.log(-window.innerHeight/4+window.pageYOffset+arr1[i].getBoundingClientRect().top)
         if(menu.style.display=="flex"){
             menu.style.display='none'
             openBtn.style.backgroundColor="#00000000";
